41a1d23d8b4e1bb939a53a9d7edde76d7f2d9b2f
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2025-07-01 22:23:22 +0200

    feat: auto-enable schedule at 9 am for pollen job

--- defs/schedules.py
+++ defs/schedules.py
@@ -1,8 +1,9 @@
-from dagster import ScheduleDefinition
+from dagster import ScheduleDefinition, DefaultScheduleStatus
 
 from defs.jobs import pollen_job
 
 pollen_job_daily_schedule = ScheduleDefinition(
     job=pollen_job,
     cron_schedule="0 9 * * *",
+    default_status=DefaultScheduleStatus.RUNNING,
 )
